a8e23a55475358c380834af01c6cf359b8c9915b
Dawn Craven <cravenew@aol.com>
2024-07-05 18:13:56 +0000

    js starter code

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var tempCelsius = 10;
 var toFahrenheit = tempCelsius * 1.8 + 32;
 
 console.log(
-    `Outside my friend's house it's ${tempCelsius} degrees Celsius, which is ${to Fahrenheit} degrees Fahrenheit.`
+  `Outside my friend's house it's ${tempCelsius} degrees Celsius, which is ${toFahrenheit} degrees Fahrenheit.`,
 );
